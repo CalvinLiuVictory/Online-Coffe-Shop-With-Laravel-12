a0d62037cca512dcc0b0175524e376693b6d74a2
CalvinLiu123 <41094560+CalvinLiu123@users.noreply.github.com>
2025-10-01 13:15:15 +0700

    Updated

---
+++ database/factories/ItemFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class ItemFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+            'name' => $this->faker->word(),
+            'category_id' => $this->faker->numberBetween(1, 2),
+            'price' => $this->faker->randomFloat(2,10000, 100000),
+            'description' => $this->faker->text(),
+            'img' => $this->faker->imageUrl(),   
+            'is_active' => $this->faker->boolean(),
+        ];
+    }
+}
