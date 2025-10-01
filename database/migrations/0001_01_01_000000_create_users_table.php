d2fc2d8dfb50b1dcf8e82f7fc796bac3895d5b0a
CalvinLiu123 <41094560+CalvinLiu123@users.noreply.github.com>
2025-10-01 14:15:34 +0700

    Updated

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $username->string('username')->unique()->fillable();
+            $table->string('username')->unique()->fillable();
             $table->string('password')->nullable();
             $table->string('fullname');
             $table->string('email')->unique()->fillable();
--- database/migrations/2025_10_01_125917_create_items_table.php
+++ database/migrations/2025_10_01_125917_create_items_table.php
@@ -13,6 +13,13 @@ return new class extends Migration
     {
         Schema::create('items', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->text('description');
+            $table->integer('price');
+            $table->unsignedBigInteger('category_id');
+            $table->string('img')->nullable();
+            $table->boolean('is_active')->default(true);
+            $table->softDeletes();
             $table->timestamps();
         });
     }
